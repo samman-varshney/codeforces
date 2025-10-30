42b917c37901fcdb6e6504351b3a4f7e0df9881f
samman-varshney <sammanvarshney345@gmail.com>
2025-10-31 02:32:06 +0530

    & 'Pinely Round 5 (Div. 1 + Div. 2)

---
+++ Pinely Round 5 (Div. 1 + Div. 2)/C.java
@@ -0,0 +1,424 @@
+import java.util.*;
+import java.io.*;
+
+public class C {
+
+    // -------------------------Boiler Code----------------------//
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    // segment tree
+    public class SegementTree {
+        int[][] tree;
+        int[] lazy;
+
+        public SegementTree(int n) {
+            tree = new int[4 * n][2];
+            lazy = new int[4 * n];
+        }
+
+        public void build(int arr[], int v, int l, int r) {
+            if (l == r) {
+                tree[v][0] = arr[l];
+                tree[v][1] = arr[l] * arr[l];
+            } else {
+                int mid = (l + r) / 2;
+                build(arr, v * 2 + 1, l, mid);
+                build(arr, v * 2 + 2, mid + 1, r);
+                tree[v][0] = tree[v * 2 + 1][0] + tree[v * 2 + 2][0];
+                tree[v][1] = tree[v * 2 + 1][1] + tree[v * 2 + 2][1];
+            }
+        }
+
+        public void removeLaziness(int v, int l, int r) {
+            if (lazy[v] != 0) {
+                int len = (r - l + 1);
+                tree[v][1] += 2 * lazy[v] * tree[v][0] + len * lazy[v] * lazy[v];
+                tree[v][0] += lazy[v] * len;
+
+                if (l != r) {
+                    lazy[v * 2 + 1] += lazy[v];
+                    lazy[v * 2 + 2] += lazy[v];
+                }
+                lazy[v] = 0;
+            }
+        }
+
+        public int query(int v, int l, int r, int ql, int qr) {
+            removeLaziness(v, l, r);
+            if (ql <= l && qr >= r) {
+                return tree[v][1];
+            } else if (ql > r || qr < l) {
+                return 0;
+            } else {
+                int mid = (l + r) / 2;
+                int left = query(v * 2 + 1, l, mid, ql, qr);
+                int right = query(v * 2 + 2, mid + 1, r, ql, qr);
+                return left + right;
+            }
+        }
+
+        public void updateRange(int v, int l, int r, int ql, int qr, int value) {
+            removeLaziness(v, l, r);
+            if (ql > r || qr < l) {
+                return;
+            }
+            if (ql <= l && qr >= r) {
+                lazy[v] += value;
+                removeLaziness(v, l, r);
+                return;
+            }
+            int mid = (l + r) / 2;
+            updateRange(v * 2 + 1, l, mid, ql, qr, value);
+            updateRange(v * 2 + 2, mid + 1, r, ql, qr, value);
+            tree[v][0] = tree[v * 2 + 1][0] + tree[v * 2 + 2][0];
+            tree[v][1] = tree[v * 2 + 1][1] + tree[v * 2 + 2][1];
+        }
+    }
+
+    // get max of an array (int)
+    public static int getMaxOfArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    // get max of an array (long)
+    public static long getMaxOfArray(long[] nums) {
+        long max = Long.MIN_VALUE;
+        for (long num : nums)
+            max = Math.max(max, num);
+        return max;
+    }
+
+    // get min of an array (int)
+    public static int getMinOfArray(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums)
+            min = Math.min(min, num);
+        return min;
+    }
+
+    // get min of an array (long)
+    public static long getMinOfArray(long[] nums) {
+        long min = Long.MAX_VALUE;
+        for (long num : nums)
+            min = Math.min(min, num);
+        return min;
+    }
+
+    // lower bound of an array
+    public static int lowerBound(int[] nums, int val) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= val) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+
+    // upper bound of an array
+    public static int upperBound(int[] nums, int val) {
+        int start = 0, end = nums.length;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= val) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    // gcd of two numbers
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // lcm of two numbers
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    // -----------------read methods-------------------------//
+    public static int readInt() throws IOException {
+        return Integer.parseInt(br.readLine().trim());
+    }
+
+    public static long readLong() throws IOException {
+        return Long.parseLong(br.readLine().trim());
+    }
+
+    public static float readFloat() throws IOException {
+        return Float.parseFloat(br.readLine().trim());
+    }
+
+    public static double readDouble() throws IOException {
+        return Double.parseDouble(br.readLine().trim());
+    }
+
+    public static String readString() throws IOException {
+        return br.readLine().trim();
+    }
+
+    public static long[] readLongArray(int n) throws IOException {
+        long[] arr = new long[n];
+        String[] tokens = br.readLine().trim().split(" ");
+        for (int i = 0; i < n; i++)
+            arr[i] = Long.parseLong(tokens[i]);
+        return arr;
+    }
+
+    public static double[] readDoubleArray(int n) throws IOException {
+        double[] arr = new double[n];
+        String[] tokens = br.readLine().trim().split(" ");
+        for (int i = 0; i < n; i++)
+            arr[i] = Double.parseDouble(tokens[i]);
+        return arr;
+    }
+
+    public static float[] readFloatArray(int n) throws IOException {
+        float[] arr = new float[n];
+        String[] tokens = br.readLine().trim().split(" ");
+        for (int i = 0; i < n; i++)
+            arr[i] = Float.parseFloat(tokens[i]);
+        return arr;
+    }
+
+    public static int[] readIntArray(int n) throws IOException {
+        int[] arr = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        return arr;
+    }
+
+    // --------------------print methods-----------------------//
+    public static void printArray(int[] arr) throws IOException {
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + (i < arr.length - 1 ? " " : ""));
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void printArray(char[] arr) throws IOException {
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0)
+                bw.write(' ');
+            bw.write(arr[i]);
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void printArray(long[] arr) throws IOException {
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0)
+                bw.write(' ');
+            bw.write(Long.toString(arr[i]));
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void printArray(float[] arr) throws IOException {
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0)
+                bw.write(' ');
+            bw.write(Float.toString(arr[i]));
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void printArray(double[] arr) throws IOException {
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0)
+                bw.write(' ');
+            bw.write(Double.toString(arr[i]));
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static <T> void printArray(T[] arr) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            if (i < arr.length - 1)
+                sb.append(" ");
+        }
+        bw.write(sb.toString());
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void println(int x) throws IOException {
+        bw.write(Integer.toString(x));
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void println(long x) throws IOException {
+        bw.write(Long.toString(x));
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void println(float x) throws IOException {
+        bw.write(Float.toString(x));
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void println(double x) throws IOException {
+        bw.write(Double.toString(x));
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void println(char x) throws IOException {
+        bw.write(x);
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void println(boolean x) throws IOException {
+        bw.write(Boolean.toString(x));
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void println() throws IOException {
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static <T> void println(T obj) throws IOException {
+        bw.write(obj.toString());
+        bw.newLine();
+        bw.flush();
+    }
+
+    public static void print(int x) throws IOException {
+        bw.write(Integer.toString(x));
+        bw.flush();
+    }
+
+    public static void print(long x) throws IOException {
+        bw.write(Long.toString(x));
+        bw.flush();
+    }
+
+    public static void print(float x) throws IOException {
+        bw.write(Float.toString(x));
+        bw.flush();
+    }
+
+    public static void print(double x) throws IOException {
+        bw.write(Double.toString(x));
+        bw.flush();
+    }
+
+    public static void print(char x) throws IOException {
+        bw.write(x);
+        bw.flush();
+    }
+
+    public static void print(boolean x) throws IOException {
+        bw.write(Boolean.toString(x));
+        bw.flush();
+    }
+
+    public static <T> void print(T obj) throws IOException {
+        bw.write(obj.toString());
+        bw.flush();
+    }
+
+    // -----------------------------------------------------//
+
+    public static void main(String[] args) {
+        try {
+            int tcase = readInt();
+            while (tcase-- > 0) {
+                int[] var = readIntArray(2);
+                long[] nums = readLongArray(var[0]);
+                helper(var[0], var[1], nums);
+            }
+        } catch (Exception err) {
+            System.out.println(err);
+        }
+    }
+
+    public static void helper(int n, int x, long[] nums) throws IOException {
+        Arrays.sort(nums);
+        long bonus = 0;
+        long sum = 0;
+        long lf = 0;
+        int i=0, j=n-1;
+        long[] res = new long[n];
+        int k = 0;
+        while(i<=j){
+            if((sum+nums[j])/x > lf){
+                res[k++] = nums[j];
+                sum += nums[j];
+                lf = sum/x;
+                bonus += nums[j];
+                j--;
+            }else{
+                sum+= nums[i];
+                res[k++] = nums[i];
+                i++;
+            }
+        }
+        println(bonus);
+        printArray(res);
+    }
+}
+
+// Example
+// InputCopy
+// 7
+// 10 2
+// 1 2 1 2 1 2 1 2 1 2
+// 5 10
+// 2 2 2 2 5
+// 11 23
+// 5 5 22 1 21 2 10 3 1 1 2
+// 1 1
+// 1
+// 1 17
+// 11
+// 3 100
+// 44 32 1
+// 16 100500
+// 42801 73112 95296 68791 42217 21871 29316 84405 24273 42894 63370 53473 57156 61369 80 27290
+// OutputCopy
+// 12
+// 1 2 2 2 2 2 1 1 1 1
+// 5
+// 2 2 2 2 5
+// 53
+// 1 1 5 2 1 2 5 3 10 21 22
+// 1
+// 1
+// 0
+// 11
+// 0
+// 44 32 1
+// 503499
+// 53473 42894 80 57156 42801 61369 42217 63370 29316 68791 27290 73112 24273 84405 21871 95296
