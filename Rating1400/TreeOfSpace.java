c00297fab9c8acbd7cb01eb65375c6cdeefb432a
samman-varshney <sammanvarshney345@gmail.com>
2026-06-12 22:53:29 +0530

    12-06-2026

--- Rating1400/TreeOfSpace.java
+++ Rating1400/TreeOfSpace.java
@@ -59,7 +59,7 @@ public class TreeOfSpace {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        sc.nextInt();
         int m = sc.nextInt();
         int q = sc.nextInt();
 
